2cf3124841cb9a4dd9a03c89fa93a7101991e9ac
yuqing630 <yu_qing630@yahoo.com>
2018-06-15 13:46:25 -0400

    Update Question 1 - sortByString.js

--- Question 1 - sortByString.js
+++ Question 1 - sortByString.js
@@ -3,10 +3,8 @@ var check = function(s,t){
   var newString = ''
   for (let i = 0; i < t.length; i++){
     while (newS.indexOf(t[i]) != -1){
-     console.log(t[i])
      newString += t[i]
      newS.splice(newS.indexOf(t[i]), 1)
-     console.log(newS)
     }
   }
   return newString
